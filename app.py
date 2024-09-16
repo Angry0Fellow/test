01aa48a816189cfb7f908807f544be53d5a5c196
Charles Clarke <cclarke002@student.butte.edu>
2024-09-15 22:19:31 -0700

    tried again

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import mysql.connector
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -18,59 +18,14 @@ def get_db_connection():
 def index():
     db_connection = get_db_connection()
     cursor = db_connection.cursor(dictionary=True)
-
+    
     # Fetch data from the 'objects' table
     cursor.execute("SELECT * FROM objects")
     objects = cursor.fetchall()
-
+    
     cursor.close()
     db_connection.close()
-
-    html = '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Inventory Management</title>
-        <style>
-            table {
-                width: 60%%;
-                border-collapse: collapse;
-                margin: 20px auto;
-            }
-            th, td {
-                border: 1px solid #dddddd;
-                text-align: left;
-                padding: 8px;
-            }
-            th {
-                background-color: #f2f2f2;
-            }
-            h2 {
-                text-align: center;
-            }
-        </style>
-    </head>
-    <body>
-        <h2>Product List</h2>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Price ($)</th>
-            </tr>
-            {% for product in products %}
-            <tr>
-                <td>{{ product['id'] }}</td>
-                <td>{{ product['name'] }}</td>
-                <td>{{ product['price'] }}</td>
-            </tr>
-            {% endfor %}
-        </table>
-    </body>
-    </html>
-    '''
-
-    return render_template_string(html, products=products)
+    return render_template('index.html', objects=objects)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
